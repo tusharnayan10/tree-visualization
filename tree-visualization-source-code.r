2e35eb5e82095cfcbc7a334b2bf9d83f06824e66
srimantkumar <40868539+srimantkumar@users.noreply.github.com>
2024-02-21 19:20:42 -0500

    Added Adjacency Treetop

--- tree-visualization-source-code.r
+++ tree-visualization-source-code.r
@@ -64,3 +64,22 @@ fig <- fig %>% layout(
 fig
 
 ##############################################################################################################
+// Code Taken from https://r-graph-gallery.com/treemap.html
+
+# library
+install.packages("treemap")
+library(treemap)
+
+# Build Dataset
+group <- c(rep("group-1",4),rep("group-2",2),rep("group-3",3))
+subgroup <- paste("subgroup" , c(1,2,3,4,1,2,1,2,3), sep="-")
+value <- c(13,5,22,12,11,7,3,1,23)
+data <- data.frame(group,subgroup,value)
+
+# treemap
+treemap(data,
+        index=c("group","subgroup"),
+        vSize="value",
+        type="index"
+) 
+
